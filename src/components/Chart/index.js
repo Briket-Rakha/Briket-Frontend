b8974daa857bcdb57764e4dea403f3cb3be4f0aa
fadhilrafiii <fadhilrafi90@gmail.com>
2021-07-11 02:09:49 +0700

    create api function to fetch hasil produksi graph data

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -2,12 +2,137 @@
 import React from 'react';
 import Chart from 'react-apexcharts';
 import PropTypes from 'prop-types';
-
-// Import Dummy
-import { dummySeries, options } from './options';
+import { dummySeries } from './dummySeries';
 
 const CustomChart = (props) => {
-  const { type, height, series } = props;
+  const { type, height, series, xtitle, ytitle } = props;
+
+  // Chart Options
+  const options= {
+    chart: {
+      type: 'bar',
+      height: 320,
+    },
+    colors: [
+      'rgba(40, 113, 148, 1)',
+      'rgba(90, 230, 150, 1)',
+      'rgba(255, 217, 54, 1)',
+      'rgba(40, 113, 148, .6)',
+      'rgba(90, 230, 150, .6)',
+      'rgba(255, 217, 54, .6)',
+      'rgba(40, 113, 148, .8)',
+      'rgba(90, 230, 150, .8)',
+      'rgba(255, 217, 54, .8)',
+    ],
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        dataLabels: {
+          position: 'top',
+        },
+      },
+    },
+    noData: {
+      text: 'No Data',
+      align: 'center',
+      verticalAlign: 'middle',
+      offsetX: 0,
+      offsetY: -44,
+      style: {
+        color: '#000',
+        fontSize: '24px',
+        fontFamily: 'Ubuntu',
+      },
+    },
+    dataLabels: {
+      enabled: false,
+      offsetX: -6,
+      style: {
+        fontSize: '12px',
+        colors: ['#fff'],
+      },
+    },
+    stroke: {
+      show: false,
+    },
+    tooltip: {
+      shared: true,
+      intersect: false,
+    },
+    xaxis: {
+      categories: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
+      labels: {
+        style: {
+          fontSize: '12px',
+          fontWeight: 400,
+          fontFamily: 'Ubuntu',
+        },
+      },
+      title: {
+        text: xtitle,
+        offsetX: 0,
+        offsetY: 0,
+        style: {
+          fontSize: '13px',
+          fontFamily: 'Ubuntu',
+          fontWeight: 500,
+          cssClass: 'apexcharts-xaxis-title',
+        },
+      },
+    },
+    title: {
+      text: ytitle,
+      align: 'left',
+      margin: 0,
+      offsetX: 0,
+      offsetY: 0,
+      floating: false,
+      style: {
+        fontSize: '13px',
+        fontWeight: 500,
+        fontFamily: 'Ubuntu',
+        color: '#263238',
+      },
+    },
+    legend: {
+      show: true,
+      position: 'bottom',
+      horizontalAlign: 'center',
+      floating: false,
+      fontSize: '14px',
+      fontFamily: 'Ubuntu',
+      fontWeight: 400,
+      inverseOrder: false,
+      offsetX: 0,
+      offsetY: 0,
+      labels: {
+        colors: undefined,
+        useSeriesColors: false,
+      },
+      markers: {
+        width: 12,
+        height: 12,
+        strokeWidth: 0,
+        strokeColor: '#fff',
+        fillColors: undefined,
+        radius: 12,
+        customHTML: undefined,
+        onClick: undefined,
+        offsetX: 0,
+        offsetY: 0,
+      },
+      itemMargin: {
+        horizontal: 5,
+        vertical: 20,
+      },
+      onItemClick: {
+        toggleDataSeries: true,
+      },
+      onItemHover: {
+        highlightDataSeries: true,
+      },
+    },
+  };
 
   return (
     <Chart
@@ -22,6 +147,8 @@ const CustomChart = (props) => {
 CustomChart.defaultProps = {
   height: 320,
   series: [],
+  xtitle: '',
+  ytitle: '',
 };
 
 CustomChart.propTypes = {
@@ -35,6 +162,8 @@ CustomChart.propTypes = {
         ),
       }),
   ),
+  xtitle: PropTypes.string,
+  ytitle: PropTypes.string,
 };
 
 export default CustomChart;
--- src/views/ProductionModel/Dashboard/FactoryProduction.js
+++ src/views/ProductionModel/Dashboard/FactoryProduction.js
@@ -1,6 +1,7 @@
 // Import Library
 import React, { useState, useEffect } from 'react';
 import { Grid } from '@material-ui/core';
+import moment from 'moment';
 
 // Import Component
 import CustomAlert from '../../../components/Alert';
@@ -12,22 +13,37 @@ import { getListOfMonths } from '../../../utils/date';
 
 // Import API
 import { apiGetPabrik } from '../../../api/pabrik.api';
+import { apiGetHasilProduksiGraph } from '../../../api/hasil-produksi.api';
 
 const FactoryProducation = () => {
   const [month, setMonth] = useState('');
   const [pabrik, setPabrik] = useState('');
-  const [series] = useState([]);
+  const [series, setSeries] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const fetchListMonths = async () => {
-    await getListOfMonths()
-        .then(console.log)
-        .catch(console.log);
+  const fetchGraphHasilProduksi = async () => {
+    const payload = {
+      pabrik,
+      month,
+      year: (new Date()).getFullYear(),
+    };
+
+    await apiGetHasilProduksiGraph(payload)
+        .then((i) => {
+          const { response: { data } } = i;
+          setSeries(data.data);
+        })
+        .catch((err) => {
+          setErrorMessage(
+              err?.message ||
+              'Gagal mengambil data hasil produksi',
+          );
+        });
   };
 
   useEffect(() => {
-    fetchListMonths();
-  });
+    if (pabrik && month) fetchGraphHasilProduksi();
+  }, [month, pabrik]);
 
   return (
     <Grid container className="dashboard-section" direction="column">
@@ -65,7 +81,12 @@ const FactoryProducation = () => {
         </Grid>
       </Grid>
       <Grid item className="dashboard-section-content">
-        <CustomChart series={series} type="bar" />
+        <CustomChart
+          series={series}
+          type="bar"
+          ytitle="Jumlah (kg)"
+          xtitle={month && moment.months(month - 1)}
+        />
       </Grid>
     </Grid>
   );
